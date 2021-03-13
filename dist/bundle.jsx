ffc1cfcc3350aa0ee7d3623a6872a18b14ad9912
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-03-13 16:44:54 -0300

    Add changes

---
+++ dist/bundle.jsx
@@ -0,0 +1,9 @@
+"use strict";
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+var _App = require("./App");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _reactDom["default"])(_App.App, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src','index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -10,6 +12,15 @@ module.exports = {
     resolve: {
         extensions: ['.js' ,'.jsx'],
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname,'public', 'index.html')
+        })
+    ],
+    
     module: {
         rules: [
             {
