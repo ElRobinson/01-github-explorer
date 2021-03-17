9fbf74782e0eeeecde13e4b7f5ae2e89be895d8a
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-03-16 21:37:26 -0300

    Added Fast Refresh and Counter component

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -18,17 +19,25 @@ module.exports = {
         contentBase: path.resolve(__dirname, 'public'),
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new htmlWebpackPlugin({
             template: path.resolve(__dirname,'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
     
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader' 
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                      plugins: [
+                          isDevelopment && require.resolve('react-refresh/babel')
+                      ].filter(Boolean)
+                  }   
+                },  
             },
             {
                 test: /\.scss$/,
