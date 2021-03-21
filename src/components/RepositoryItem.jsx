c116925a6b35302e0d2301812ab6d8b27be907d2
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-03-21 19:42:08 -0300

    Repository list now will call a github service

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props){
         <li>
         <strong>{props.repository?.name ?? 'Default'}</strong>
         <p>{props.repository.description}</p>
-        <a href={props.repository.link}>
+        <a href={props.repository.html_url}>
             Acessar repositórios
         </a>
     </li>
